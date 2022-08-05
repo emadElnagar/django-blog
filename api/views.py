05ab563dbe5196ee18b38db5269c4dc553a7a843
unknown <emad.m.alnagar@gmail.com>
2022-08-05 14:38:08 +0200

    create a new comment

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http.response import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import PostsSerializer, CommentsSerializer
 from blog.models import Post, Comment
 
@@ -19,10 +20,19 @@ def SinglePost(request, slug):
     serializer = PostsSerializer(post, many = False)
     return Response(serializer.data)
 
-# GET POST COMMENTS
-@api_view(['GET'])
+# POST COMMENTS VIEW
+@api_view(['GET', 'POST'])
 def PostComments(request, slug):
+    # GET COMMENTS OF POST
     post = Post.objects.get(slug=slug)
-    comments = Comment.objects.filter(post = post)
-    serializer = CommentsSerializer(comments, many = True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        comments = Comment.objects.filter(post = post)
+        serializer = CommentsSerializer(comments, many = True)
+        return Response(serializer.data)
+    # CREATE A NEW COMMENT
+    elif request.method == 'POST':
+        serializer = CommentsSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
